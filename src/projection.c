a8b756500c01ab55b7d5a2ca9ef1c47770a3b4ab
Damien DIDIER <damien.didier@epita.fr>
2022-12-05 17:50:50 +0100

    fix(projection) remove vla (variable-length array) usage in main and projection, created functions to initialise the differents matrix used for projection

--- src/projection.c
+++ src/projection.c
@@ -7,31 +7,30 @@
 #include "rasterizer.h"
 #include "vector3.h"
 
-void init_z_buffer(size_t height, size_t width, float *dst)
+float *z_buffer_init(size_t height, size_t width)
 {
     static float *z_buffer = NULL;
     if (z_buffer == NULL)
     {
         z_buffer = malloc(height * width * sizeof(float));
         for (size_t i = 0; i < width * height; i++)
+        {
             z_buffer[i] = 1.0f;
+        }
     }
-    memcpy(dst, z_buffer, height * width * sizeof(float));
+
+    float *dest = malloc(height * width * sizeof(float));
+    return memcpy(dest, z_buffer, height * width * sizeof(float));
 }
 
-int projection(obj_t *object, camera_t *camera, vector3_t *light, int *image)
+tmatrix_t *world_to_view_init(camera_t *camera)
 {
-    vector3_t *triangle[3];
-    vector3_t projTriangle[3];
-    face_t *face;
-    float norm;
     /*
-     * vectors used to calculate each tri normal
+     * floats used to express world_to_view
      */
-    vector3_t view = VECTOR3_INIT;
-    vector3_t v_side_1 = VECTOR3_INIT;
-    vector3_t v_side_2 = VECTOR3_INIT;
-    vector3_t v_normal = VECTOR3_INIT;
+    float pos_dot_ux;
+    float pos_dot_uy;
+    float pos_dot_uz;
 
     /*
      * vectors used to express the matrix world_to_view
@@ -43,57 +42,19 @@ int projection(obj_t *object, camera_t *camera, vector3_t *light, int *image)
     vector3_t camera_uy = VECTOR3_INIT;
     vector3_t camera_uz = *(camera->look_at);
 
-    /*
-     * floats used to express world_to_view
-     */
-    float pos_dot_ux;
-    float pos_dot_uy;
-    float pos_dot_uz;
-
-    float color;
-
-    float z_buffer[camera->height * camera->width];
-    init_z_buffer(camera->height, camera->width, z_buffer);
-
-    for (size_t i = 0; i < 3; i++)
-        projTriangle[i] = VECTOR3_INIT;
-
-    tmatrix_t *view_to_clip = malloc(sizeof(struct tmatrix_t));
-    if (view_to_clip == NULL)
-        err(EXIT_FAILURE,
-            "Error while allocating memory for view_to_clip matrix");
-    tmatrix_init(view_to_clip);
-
     tmatrix_t *world_to_view = malloc(sizeof(struct tmatrix_t));
     if (world_to_view == NULL)
         err(EXIT_FAILURE,
             "Error while allocating memory for world_to_view matrix");
     tmatrix_init(world_to_view);
 
-    tmatrix_t *projMatrix = malloc(sizeof(struct tmatrix_t));
-    if (projMatrix == NULL)
-        err(EXIT_FAILURE,
-            "Error while allocating memory for projMatrix matrix");
-    tmatrix_init(projMatrix);
-
-    float aspectRatio = (float)camera->height / (float)camera->width;
-    float zNear = 0.1f;
-    float zFar = 1000.0f;
-    float fovRad = 1.0f / tanf(camera->fov * 0.5f / 180.0f * M_PI);
-
-    view_to_clip->data[0] = aspectRatio * fovRad;
-    view_to_clip->data[5] = fovRad;
-    view_to_clip->data[10] = zFar / (zFar - zNear);
-    view_to_clip->data[11] = 1.0f;
-    view_to_clip->data[14] = (-zFar * zNear) / (zFar - zNear);
-
     /*
      * Calculate the other vectors of the camera base (right/ux and down/uy)
      * Algo not adapted to vertical camera : need some clamping
      */
     v_cross_v(&camera_uz, &uz, &camera_ux);
-    norm = fastisqrt(camera_ux.x * camera_ux.x + camera_ux.y * camera_ux.y
-                     + camera_ux.z * camera_ux.z);
+    float norm = fastisqrt(camera_ux.x * camera_ux.x + camera_ux.y * camera_ux.y
+                           + camera_ux.z * camera_ux.z);
     camera_ux.x = camera_ux.x * norm;
     camera_ux.y = camera_ux.y * norm;
     camera_ux.z = camera_ux.z * norm;
@@ -125,7 +86,77 @@ int projection(obj_t *object, camera_t *camera, vector3_t *light, int *image)
     world_to_view->data[14] = -pos_dot_uz;
     world_to_view->data[15] = 1;
 
-    tm_dot_tm(world_to_view, view_to_clip, projMatrix);
+    return world_to_view;
+}
+
+tmatrix_t *view_to_clip_init(camera_t *camera)
+{
+    tmatrix_t *view_to_clip = malloc(sizeof(struct tmatrix_t));
+    if (view_to_clip == NULL)
+        err(EXIT_FAILURE,
+            "Error while allocating memory for view_to_clip matrix");
+    tmatrix_init(view_to_clip);
+
+    float aspectRatio = (float)camera->height / (float)camera->width;
+    float zNear = 0.1f;
+    float zFar = 1000.0f;
+    float fovRad = 1.0f / tanf(camera->fov * 0.5f / 180.0f * M_PI);
+
+    view_to_clip->data[0] = aspectRatio * fovRad;
+    view_to_clip->data[5] = fovRad;
+    view_to_clip->data[10] = zFar / (zFar - zNear);
+    view_to_clip->data[11] = 1.0f;
+    view_to_clip->data[14] = (-zFar * zNear) / (zFar - zNear);
+
+    return view_to_clip;
+}
+
+int projection(obj_t *object, camera_t *camera, vector3_t *light, int *image)
+{
+    //**************************************************************************
+    // Start initialisation
+    //**************************************************************************
+
+    /*============================= z_buffer =================================*/
+
+    float *z_buffer = z_buffer_init(camera->height, camera->width);
+
+    /*========================== object_to_world =============================*/
+    /*=========================== world_to_view ==============================*/
+
+    tmatrix_t *world_to_view = world_to_view_init(camera);
+
+    /*=========================== view_to_clip ===============================*/
+
+    tmatrix_t *view_to_clip = view_to_clip_init(camera);
+
+    /*============================ proj_matrix ===============================*/
+
+    tmatrix_t *proj_matrix = malloc(sizeof(struct tmatrix_t));
+    if (proj_matrix == NULL)
+        err(EXIT_FAILURE,
+            "Error while allocating memory for proj_matrix matrix");
+    tmatrix_init(proj_matrix);
+
+    tm_dot_tm(world_to_view, view_to_clip, proj_matrix);
+
+    /*=============================== other ==================================*/
+
+    face_t *face;
+    vector3_t *triangle[3];
+    vector3_t proj_triangle[3] = { VECTOR3_INIT, VECTOR3_INIT, VECTOR3_INIT };
+
+    /*
+     * vectors used to calculate each tri normal
+     */
+    vector3_t view = VECTOR3_INIT;
+    vector3_t v_side_1 = VECTOR3_INIT;
+    vector3_t v_side_2 = VECTOR3_INIT;
+    vector3_t v_normal = VECTOR3_INIT;
+
+    //**************************************************************************
+    // End initialisation
+    //**************************************************************************
 
     for (size_t face_i = 0; face_i < object->face_count; face_i++)
     {
@@ -152,8 +183,8 @@ int projection(obj_t *object, camera_t *camera, vector3_t *light, int *image)
         v_cross_v(&v_side_1, &v_side_2, &v_normal);
 
         // normalize
-        norm = fastisqrt(v_normal.x * v_normal.x + v_normal.y * v_normal.y
-                         + v_normal.z * v_normal.z);
+        float norm = fastisqrt(v_normal.x * v_normal.x + v_normal.y * v_normal.y
+                               + v_normal.z * v_normal.z);
         v_normal.x = v_normal.x * norm;
         v_normal.y = v_normal.y * norm;
         v_normal.z = v_normal.z * norm;
@@ -169,30 +200,34 @@ int projection(obj_t *object, camera_t *camera, vector3_t *light, int *image)
             continue;
 
         // projection View -> Clipping space
-        v_dot_m(triangle[0], projMatrix, projTriangle + 0);
-        v_dot_m(triangle[1], projMatrix, projTriangle + 1);
-        v_dot_m(triangle[2], projMatrix, projTriangle + 2);
+        v_dot_m(triangle[0], proj_matrix, proj_triangle + 0);
+        v_dot_m(triangle[1], proj_matrix, proj_triangle + 1);
+        v_dot_m(triangle[2], proj_matrix, proj_triangle + 2);
 
         // Clipping space -> Screen space
         for (size_t i = 0; i < 3; i++)
         {
-            projTriangle[i].x = 0.5f * (projTriangle[i].x + 1) * (float)camera->width;
-            projTriangle[i].y = 0.5f * (projTriangle[i].y + 1) * (float)camera->height;
+            proj_triangle[i].x =
+                0.5f * (proj_triangle[i].x + 1) * (float)camera->width;
+            proj_triangle[i].y =
+                0.5f * (proj_triangle[i].y + 1) * (float)camera->height;
         }
 
         // Proto coloration
+        float color;
         v_dot_v(light, &v_normal, &color);
         color = min(color, 255);
         color = max(1, color);
 
         // Raster stage
-        build_triangle(projTriangle, projTriangle + 1, projTriangle + 2,
+        build_triangle(proj_triangle, proj_triangle + 1, proj_triangle + 2,
                        (int)color, image, z_buffer, camera->width,
                        camera->height);
     }
 
     free(view_to_clip);
     free(world_to_view);
-    free(projMatrix);
+    free(proj_matrix);
+    free(z_buffer);
     return 0;
 }
